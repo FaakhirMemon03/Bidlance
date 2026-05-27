38dd8fa4dd404688d560234e3a7be48e9024dd92
FaakhirMemon030 <alexwatson031105@gmail.com>
2026-05-28 04:40:39 +0500

    feat: implement project routes with CRUD operations and role-based authorization

--- server/src/routes/projectRoutes.js
+++ server/src/routes/projectRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getProjects, getProject, createProject, updateProject, deleteProject } = require('../controllers/projectController');
+const { getProjects, getProject, createProject, updateProject, deleteProject, getSellerProjects } = require('../controllers/projectController');
 const { protect, authorize } = require('../middlewares/authMiddleware');
 
 router.get('/', getProjects);
+router.get('/my-projects', protect, authorize('seller'), getSellerProjects);
 router.get('/:id', getProject);
 router.post('/', protect, authorize('seller', 'admin'), createProject);
 router.put('/:id', protect, updateProject);
